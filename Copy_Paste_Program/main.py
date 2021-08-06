1da3772c7388e0a16de35ed08c53f4c2386b5a88
Max Marshall <40341744+Jormungandr1105@users.noreply.github.com>
2021-08-06 03:25:47 -0400

    First pass getting started

---
+++ Copy_Paste_Program/main.py
@@ -0,0 +1,15 @@
+import tkinter
+
+
+def submit_paste():
+	# Send packet to other computer
+	pass
+
+def set_target_address():
+	# Set IP for connected computer
+	pass
+
+
+if __name__ == '__main__':
+	m = tkinter.Tk()
+	m.mainloop()
